43bbdc458b39cfb8d694057cde3381691286670e
DeProgrammer <masonem99@hotmail.com>
2024-12-22 18:27:06 -0600

    Fix a bug in the winnings multiplier fix

--- src/minigame/MinigameUtil.ts
+++ src/minigame/MinigameUtil.ts
@@ -12,14 +12,14 @@ export const OnePracticeRun = [{ type: getResourceType(PracticeRuns), amount: 1 
 //Added outputMultiplier so you can apply a factor before rounding without writing "multiplier * " twice for every call to one of these functions.
 export function rangeMapLinear(value: number, lowEndOutput: number, highEndOutput: number, rangeLower: number, rangeUpper: number, roundTo: number, outputMultiplier: number = 1) {
     if (value < rangeLower) return 0;
-    if (value >= rangeUpper) return highEndOutput;
+    if (value >= rangeUpper) return highEndOutput * outputMultiplier;
     return Math.round((lowEndOutput + (highEndOutput - lowEndOutput) * (value - rangeLower) / (rangeUpper - rangeLower)) * outputMultiplier / roundTo) * roundTo;
 }
 
 //Same thing, but with a curve-bending factor (exponent, but the number is 0-1, so if exponent > 1, the output approaches the maximum faster at the lower end).
 export function rangeMapExp(value: number, lowEndOutput: number, highEndOutput: number, rangeLower: number, rangeUpper: number, exponent: number, roundTo: number, outputMultiplier: number = 1) {
     if (value < rangeLower) return 0;
-    if (value >= rangeUpper) return highEndOutput;
+    if (value >= rangeUpper) return highEndOutput * outputMultiplier;
     return Math.round((lowEndOutput + (highEndOutput - lowEndOutput) * Math.pow((value - rangeLower) / (rangeUpper - rangeLower), exponent)) * outputMultiplier / roundTo) * roundTo;
 }
 
